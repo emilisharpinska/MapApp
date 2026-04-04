641acb94b56514b5396a5e908ec7a1a242535f17
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-04-04 19:05:05 +0200

    [Feature] add the id for locations

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,25 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Emili Sharpinska on 02/04/2026.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location: Identifiable {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+    var id: String {
+        name + cityName
+    }
+}
+
+

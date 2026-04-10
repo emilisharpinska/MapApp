54cbea3186ef9e970ef1cb0932cb43c31d0c998d
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-04-10 07:29:54 +0200

    [Feature] Add equatable protocol to model

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -9,7 +9,8 @@ import Foundation
 import MapKit
 
 
-struct Location: Identifiable {
+struct Location: Identifiable, Equatable {
+    
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
@@ -20,6 +21,12 @@ struct Location: Identifiable {
     var id: String {
         name + cityName
     }
+    
+    //Equatable
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+    
 }
 
 

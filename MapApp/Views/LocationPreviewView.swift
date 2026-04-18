1a2625381f03f91b191a33313b4582ac63b7fce2
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-04-18 13:29:05 +0200

    [Feature] Add location to LocationPreviewView

--- MapApp/Views/LocationPreviewView.swift
+++ MapApp/Views/LocationPreviewView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct LocationPreviewView: View {
+    
+    let location: Location
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LocationPreviewView()
+    LocationPreviewView(location: LocationsDataService.locations.first!)
 }
